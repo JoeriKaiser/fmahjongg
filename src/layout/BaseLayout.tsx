e2932686b4f924f275fda913323800c9ec424ded
JoeriKaiser <joeri.kaiser@hotmail.com>
2024-11-09 00:08:03 +0100

    fix(layout): Temp disable sidebar

--- src/layout/BaseLayout.tsx
+++ src/layout/BaseLayout.tsx
@@ -1,14 +1,11 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/ui/app-sidebar"
-
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider open={false}>
+    <>
+      {/* <SidebarProvider open={false}>
       <AppSidebar />
-      <main>
-        <SidebarTrigger />
-        {children}
-      </main>
-    </SidebarProvider>
-  )
+      <SidebarTrigger /> */}
+      {children}
+      {/* </SidebarProvider> */}
+    </>
+  );
 }
